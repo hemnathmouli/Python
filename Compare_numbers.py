7b2f4621f8fa5d7d76457f1ffbc94d3362859c3b
hemnathmouli <hemnathmouli@gmail.com>
2015-04-02 17:10:05 +0530

    Compare two numbers
    Simple program to compare two numbers

---
+++ Compare_numbers.py
@@ -0,0 +1,11 @@
+print ("Comparing two number: ")
+n1=input("Enter 1st number: ")
+n2=input("Enter 2nd number: ")
+if (n1>n2):
+    print ('The number',n1,'is greater than ',n2)
+elif (n1==n2):
+    print('The two numbers', n1,'and',n2,'are equal')
+elif (n1<n2):
+    print ('The number',n1,'is lesser than ',n2)
+input("Press Enter")
+       
