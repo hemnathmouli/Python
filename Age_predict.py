de78da7c99b2bbefa80f1577a7cb10084fe3beb8
hemnathmouli <hemnathmouli@gmail.com>
2015-04-02 17:05:41 +0530

    Age prediction
    Predict your age after or before your current age

---
+++ Age_predict.py
@@ -0,0 +1,8 @@
+print ("Age pridictor")
+iage_str=input("Enter your current age: ")
+iage=int(iage_str)
+iyear_str=input("Enter the year you wanted to pridict your age: ")
+year=int(iyear_str)
+fage=year-2015+iage
+print (fage)
+input("Press Enter")
