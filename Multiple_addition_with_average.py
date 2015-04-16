719c278372029b942adf44d042df545556d11fac
hemnathmouli <hemnathmouli@gmail.com>
2015-04-16 09:56:30 +0530

    Multiple_addition.py
    With this program, we can find the addition of 'n' number and can ever added up extra future as finding its average.

---
+++ Multiple_addition_with_average.py
@@ -0,0 +1,69 @@
+raw=input("Enter the number 2 to 6:")
+if(raw=='2'):
+    n1_str=input("n1: ")
+    n1=int(n1_str)
+    n2_str=input("n2: ")
+    n2=int(n2_str)
+    n3=0
+    n4=0
+    n5=0
+    n6=0
+elif(raw=='3'):
+    n1_str=input("n1: ")
+    n1=int(n1_str)
+    n2_str=input("n2: ")
+    n2=int(n2_str)
+    n3_str=input("n3: ")
+    n3=int(n3_str)
+    n4=0
+    n5=0
+    n6=0
+
+elif(raw=='4'):
+    n1_str=input("n1: ")
+    n1=int(n1_str)
+    n2_str=input("n2: ")
+    n2=int(n2_str)
+    n3_str=input("n3: ")
+    n3=int(n3_str)
+    n4_str=input("n4: ")
+    n4=int(n4_str)
+    n5=0
+    n6=0
+
+elif(raw=='5'):
+    n1_str=input("n1: ")
+    n1=int(n1_str)
+    n2_str=input("n2: ")
+    n2=int(n2_str)
+    n3_str=input("n3: ")
+    n3=int(n3_str)
+    n4_str=input("n4: ")
+    n4=int(n4_str)
+    n5_str=input("n5: ")
+    n5=int(n5_str)
+    n6=0
+elif(raw=='6'):
+    n1_str=input("n1: ")
+    n1=int(n1_str)
+    n2_str=input("n2: ")
+    n2=int(n2_str)
+    n3_str=input("n3: ")
+    n3=int(n3_str)
+    n4_str=input("n4: ")
+    n4=int(n4_str)
+    n5_str=input("n5: ")
+    n5=int(n5_str)
+    n6_str=input("n6: ")
+    n6=int(n6_str)
+n_str=n1+n2+n3+n4+n5+n6
+n=int(n_str)
+print ("The addition of the entered number is",n)
+entery=input("Do you want to find the out put of this numbers?:[y/n]")
+if(entery=='y'):
+    raw1=int(raw)
+    avg=n/raw1
+    print ("The average of the numbers entered is", avg)
+    input("PressEnter")
+else:
+    input("PressEnter")
